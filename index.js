83040b029f961531cf8f0796e60204ae570f0bf3
trhinehart <trhinehart@godaddy.com>
2019-12-03 11:09:36 -0700

    hoist getName/getDynamicWrapper definitions to top

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const {createFilter} = require("rollup-pluginutils");
 const importToGlobals = require("./lib/import-to-globals");
 
 function createPlugin(globals, {include, exclude, dynamicWrapper = "Promise.resolve"} = {}) {
+  if (!globals) {
+    throw new TypeError("Missing mandatory option 'globals'");
+  }
+  let getName;
+  const globalsType = typeof globals;
+  const isGlobalsObj = globalsType === "object";
+  if (isGlobalsObj) {
+    getName = function (name) {
+      if (globals.hasOwnProperty(name)) {
+        return globals[name];
+      }
+    };
+  } else if (globalsType === "function") {
+    getName = globals;
+  } else {
+    throw new TypeError(`Unexpected type of 'globals', got '${globalsType}'`);
+  }
+  let getDynamicWrapper;
+  const dynamicWrapperType = typeof dynamicWrapper;
+  if (dynamicWrapperType === "function") {
+    getDynamicWrapper = dynamicWrapper;
+  } else if (dynamicWrapperType === "string") {
+    getDynamicWrapper = function (name) {
+      return `${dynamicWrapper}(${name})`;
+    };
+  } else {
+    throw new TypeError(`Unexpected type of 'dynamicWrapper', got '${dynamicWrapperType}'`);
+  }
   const filter = createFilter(include, exclude);
   return {
     name: "rollup-plugin-external-globals",
@@ -11,38 +39,8 @@ function createPlugin(globals, {include, exclude, dynamicWrapper = "Promise.reso
   };
 
   function transform(code, id) {
-    if (!globals) {
-      throw new TypeError("Missing mandatory option 'globals'");
-    }
-    if ((id[0] !== "\0" && !filter(id))) {
+    if ((id[0] !== "\0" && !filter(id)) || (isGlobalsObj && Object.keys(globals).every(id => !code.includes(id)))) {
       return;
-    }
-    let getName;
-    const globalsType = typeof globals;
-    if (globalsType === "function") {
-      getName = globals;
-    } else if (globalsType === "object") {
-      if (Object.keys(globals).every(id => !code.includes(id))) {
-        return;
-      }
-      getName = function (name) {
-        if (globals.hasOwnProperty(name)) {
-          return globals[name];
-        }
-      };
-    } else {
-      throw new TypeError(`Unexpected type of 'globals', got '${globalsType}'`);
-    }
-    let getDynamicWrapper;
-    const dynamicWrapperType = typeof dynamicWrapper;
-    if (dynamicWrapperType === "function") {
-      getDynamicWrapper = dynamicWrapper;
-    } else if (dynamicWrapperType === "string") {
-      getDynamicWrapper = function (name) {
-        return `${dynamicWrapper}(${name})`;
-      };
-    } else {
-      throw new TypeError(`Unexpected type of 'dynamicWrapper', got '${dynamicWrapperType}'`);
     }
     const ast = this.parse(code);
     code = new MagicString(code);
