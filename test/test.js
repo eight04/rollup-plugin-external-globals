516c2ed791623f0908e22b2922f7415773583860
eight04 <eight04@gmail.com>
2020-10-21 14:28:09 +0800

    Fix: we need an extra assignment to export globals

--- test/test.js
+++ test/test.js
@@ -299,7 +299,7 @@ describe("main", () => {
   );
 
   // https://github.com/acornjs/acorn/issues/806
-  xit("export from default", () =>
+  it("export from default", () =>
     withDir(`
       - entry.js: |
           export {default as baz} from "bak";
@@ -310,7 +310,10 @@ describe("main", () => {
         boo: "BOO",
       });
       assert.equal(code.trim(), endent`
-        export { BAK as baz, BOO };
+        const _global_BAK = BAK;
+        const _global_BOO = BOO;
+      
+        export { _global_BOO as BOO, _global_BAK as baz };
       `);
     })
   );
